c7b263d0b673da5b3317ffadca22cffa01d6fdb7
Bouain Kamel <114147593+kamelguide@users.noreply.github.com>
2026-05-12 19:35:08 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const API_KEY = "1aa3b3a2a30479c44d2b14e77bed9a07";
+const BASE_URL ="http://api.openweathermap.org/data/2.5/weather?appid=" + API_KEY + "&q=metnasser,tn&units=metric";
+
+function getWeatherData(city, callback) {
+const url = BASE_URL + city;
+
+fetch(url)
+.then((res) => res.json())
+.then((weatherData) => callback(null, weatherData))
+.catch((error) => callback(error, null));
+}
+function displayWeatherData(city) {
+getWeatherData(city, (error, weatherData) => {
+if (error) {
+console.error("Error fetching weather data:", error);
+return;
+
+}
+console.log("Weather data for", weatherData.name + ":", weatherData.main.temp, + "°C");
+});}
+
+displayWeatherData("sousse");
